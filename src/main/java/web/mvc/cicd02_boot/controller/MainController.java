3715dd0e0343d0b07c57cee5b1149250819b1053
blueberrylatte <yoffle0987@gmail.com>
2025-07-10 10:47:21 +0900

    빌드 실패 테스트

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-            return "GitHub Actions를 테스트 CI/CD TEST";
+            return "GitHub Actions 빌드 실패 시 배포 중단  CI/CD TEST";
     }
 }
